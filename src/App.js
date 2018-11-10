945e140e8f1a4dd5fd56ead0caca08779474d8a9
Nabid-Kaisar <himelkaisar023@gmail.com>
2018-11-10 16:23:54 +0600

    added fetures: full screen, picutre in picture

--- src/App.js
+++ src/App.js
@@ -8,14 +8,18 @@ class App extends Component {
     super(props);
     this.state = {
       // url: "http://techslides.com/demos/sample-videos/small.mp4"
-      url: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
+      //url: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
+      url: "https://mnmedias.api.telequebec.tv/m3u8/29880.m3u8"
     };
 
-    this.playPause = this.playPause.bind(this);
-    this.muteVolume = this.muteVolume.bind(this);
+    this.playPause = this.handlePlayPause.bind(this);
+    this.muteVolume = this.handleMuteVolume.bind(this);
+    this.handleSetting = this.handleSetting.bind(this);
+    this.handleMinMax = this.handleMinMax.bind(this);
+    this.handlePip = this.handlePip.bind(this);
   }
 
-  async playPause() {
+  async handlePlayPause() {
     var video = document.getElementById("video");
     if (video.paused) {
       video.play();
@@ -23,21 +27,20 @@ class App extends Component {
       video.pause();
     }
     var playPauseButton = document.getElementById("play-pause-button");
-    console.log(playPauseButton.classList.value)
+    console.log(playPauseButton.classList.value);
 
     //checking the current className and toggling
 
-    if(playPauseButton.classList.value === "play-button"){
+    if (playPauseButton.classList.value === "play-button") {
       playPauseButton.classList.remove("play-button");
       playPauseButton.classList.add("pause-button");
-    }else{
+    } else {
       playPauseButton.classList.remove("pause-button");
-      playPauseButton.classList.add("play-button")
+      playPauseButton.classList.add("play-button");
     }
-
   }
 
-  muteVolume() {
+  handleMuteVolume() {
     var video = document.getElementById("video");
     if (video.muted) {
       video.muted = false;
@@ -47,18 +50,59 @@ class App extends Component {
 
     //checking the current className and toggling
     var muteUnmuteButton = document.getElementById("mute-unmute-button");
-    if(muteUnmuteButton.classList.value === "unmute-button"){
+    if (muteUnmuteButton.classList.value === "unmute-button") {
       muteUnmuteButton.classList.remove("unmute-button");
-      muteUnmuteButton.classList.add("mute-button")
-    }else{
+      muteUnmuteButton.classList.add("mute-button");
+    } else {
       muteUnmuteButton.classList.remove("mute-button");
-      muteUnmuteButton.classList.add("unmute-button")
+      muteUnmuteButton.classList.add("unmute-button");
     }
   }
 
-  componentDidMount() {
-    var video = document.getElementById("video");
+  handleSetting() {
+    //fetch current video quality and show it in here..
 
+    var dropdown = document.getElementById("quality-drop-down");
+    dropdown.classList.toggle("show");
+    //clicking on outside closes the dropdown window
+    window.onclick = function(event) {
+      if (!event.target.matches(".dropbtn")) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains("show")) {
+            openDropdown.classList.remove("show");
+          }
+        }
+      }
+    };
+  }
+
+  handleMinMax() {
+    var video = document.getElementById("video");
+    //checking if video running full screen or not
+    if(!video.webkitDisplayingFullscreen){
+      if (video.requestFullscreen) video.requestFullscreen();
+      //adding supports for all browsers
+      else if (video.msRequestFullscreen) video.msRequestFullscreen();
+      else if (video.mozRequestFullScreen) video.mozRequestFullScreen();
+      else if (video.webkitRequestFullscreen) video.webkitRequestFullscreen();
+    }else{
+      //*** have to check other browser's compatibility
+      document.webkitExitFullscreen();
+    }
+  }
+
+  handlePip(){
+    var video = document.getElementById("video");
+    video.requestPictureInPicture();
+  }
+
+
+  componentDidMount() {
+    //using hls.js to convert m3u8 format to mp4
+    var video = document.getElementById("video");
     if (Hls.isSupported()) {
       var hls = new Hls();
       hls.loadSource(this.state.url);
@@ -77,18 +121,44 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <video id="video" width="400" controls/>
+        <video id="video" width="400" controls autoPlay />
 
         <br />
-        <div className = "control-button">
-        <button id ="play-pause-button" className="play-button" onClick={this.playPause}>
-        </button>
-        <button id= "mute-unmute-button" className="unmute-button" onClick={this.muteVolume}>
-        </button>
+        <div className="control-button">
+          <button
+            id="play-pause-button"
+            className="pause-button"
+            onClick={this.handlePlayPause}
+          />
+          <button
+            id="mute-unmute-button"
+            className="unmute-button"
+            onClick={this.handleMuteVolume}
+          />
+          <button
+            id="setting"
+            className="dropbtn"
+            onClick={this.handleSetting}
+          />
+          <div id="quality-drop-down" className="dropdown-content">
+            <ul>360p</ul>
+            <ul>480p</ul>
+            <ul>720p</ul>
+          </div>
+          <button
+            id="minimize-maximize"
+            className="maximize-button"
+            onClick={this.handleMinMax}
+          />
+          <button id = "pip"
+          className = "pip-button"
+          onClick = {this.handlePip}
+          />
         </div>
+
         <div>
           <h5>Select Video Quality</h5>
-          <select>
+          <select id="setting-button">
             <option value="360p">360p</option>
             <option value="480p">480p</option>
             <option value="720p">720p</option>
